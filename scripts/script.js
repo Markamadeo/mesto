0e6e3c3c45bd3a6dd3376333088809cc430b7c24
Artem Kulakov <kulakov22@yandex.ru>
2020-08-28 17:23:43 +0700

    fix: исправить баги после 2-го ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 const editForm = document.querySelector(".edit-form");
 const profileEditButton = document.querySelector(".profile__edit-botton");
-const editFormFullName = document.querySelector(".edit-form__textinput_full-name");
-const editFormDescription = document.querySelector(".edit-form__textinput_description");
+const editFormFullName = document.querySelector(".edit-form__textinput_type_full-name");
+const editFormDescription = document.querySelector(".edit-form__textinput_type_description");
 const fullNameOnPage = document.querySelector(".profile__full-name");
 const descriptionOnPage = document.querySelector(".profile__description");
 const editFormContainer = document.querySelector(".edit-form__container");
@@ -9,23 +9,23 @@ const closeButton = document.querySelector(".edit-form__close-button");
 const saveButton = document.querySelector(".edit-form__save-button");
 
 const openForm = () => {
-  editForm.classList.toggle("edit-form_status_active");
-  editFormFullName.focus();
   editFormFullName.value = fullNameOnPage.textContent;
   editFormDescription.value = descriptionOnPage.textContent;
-}
-
-const saveProfile = (event) => {
-  event.preventDefault();
-  fullNameOnPage.textContent = editFormFullName.value;
-  descriptionOnPage.textContent = editFormDescription.value;
-  closeForm();
+  editForm.classList.toggle("edit-form_status_active");
+  editFormFullName.focus();
 }
 
 const closeForm = (event) => {
   if (event.target === event.currentTarget || event.target === saveButton) {
     editForm.classList.toggle("edit-form_status_active");
   }
+}
+
+const saveProfile = (event) => {
+  event.preventDefault();
+  fullNameOnPage.textContent = editFormFullName.value;
+  descriptionOnPage.textContent = editFormDescription.value;
+  closeForm(event);
 }
 
 profileEditButton.addEventListener('click', openForm);
