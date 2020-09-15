4f9945685743ee3b894976512c09e2bcc0e4e1bc
Artem Kulakov <kulakov22@yandex.ru>
2020-09-15 20:55:47 +0700

    hotfix: Исправить замечания после ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,132 +1,107 @@
 import { initialCards } from './data.js';
+
 const gallery = document.querySelector('.gallery');
 const galleryItem = document.querySelector('#gallery-item').content;
-
-/* POPUP: Edit form */
-const editForm = document.querySelector('.edit-form');
+const fotoViewerImg = document.querySelector('.form__foto-viewer-img');
+const fotoCaption = document.querySelector('.form__foto-viewer-description');
 const profileEditButton = document.querySelector('.profile__edit-botton');
-const editFormFullName = document.querySelector('.edit-form__textinput_type_full-name');
-const editFormDescription = document.querySelector('.edit-form__textinput_type_description');
+const profileAddingButton = document.querySelector('.profile__add-botton');
+const editForm = document.querySelector('.form_type_edit');
+const addingForm = document.querySelector('.form_type_adding');
+const fotoViewer = document.querySelector('.form_type_foto-viewer');
+const editFormFullName = document.querySelector('.form__textinput_type_edit-full-name');
+const editFormDescription = document.querySelector('.form__textinput_type_edit-description');
 const fullNameOnPage = document.querySelector('.profile__full-name');
 const descriptionOnPage = document.querySelector('.profile__description');
-const editFormContainer = document.querySelector('.edit-form__container');
-const closeEditButton = document.querySelector('.edit-form__close-button');
-const saveEditButton = document.querySelector('.edit-form__save-button');
+const editSaveButton = document.querySelector('.form__submit-button_type_edit-save-button');
+const addCardButton = document.querySelector('.form__submit-button_type_adding-add-button');
+const addingFormName = document.querySelector('.form__textinput_type_adding-name');
+const addingFormLinkAdress = document.querySelector('.form__textinput_type_adiing-link-adress');
 
-const openEditForm = () => {
+const toggleClass = (elem) => {
+  elem.classList.toggle('form_status_active');
+};
+
+const formFillValues = () => {
   editFormFullName.value = fullNameOnPage.textContent;
   editFormDescription.value = descriptionOnPage.textContent;
-  editForm.classList.toggle('edit-form_status_active');
   editFormFullName.focus();
 };
 
-const closeEditForm = (event) => {
-  if (event.target === event.currentTarget || event.target === saveEditButton || event.target === closeEditButton) {
-    editForm.classList.toggle('edit-form_status_active');
-  }
-};
-
-const saveProfile = (event) => {
-  event.preventDefault();
-  fullNameOnPage.textContent = editFormFullName.value;
-  descriptionOnPage.textContent = editFormDescription.value;
-};
-
-profileEditButton.addEventListener('click', openEditForm);
-editFormContainer.addEventListener('submit', saveProfile);
-editForm.addEventListener('click', closeEditForm);
-/* POPUP: Edit form - END */
-
-/* POPUP: Adding cards form */
-const addingForm = document.querySelector('.adding-form');
-const profileAddingButton = document.querySelector('.profile__add-botton');
-const addingFormName = document.querySelector('.adding-form__textinput_type_name');
-const addingFormLinkAdress = document.querySelector('.adding-form__textinput_type_link-adress');
-const addingFormAddButton = document.querySelector('.adding-form__add-button');
-const addingFormCloseButton = document.querySelector('.adding-form__close-button');
-const addingFormContainer = document.querySelector('.adding-form__container');
-
-const openAddingForm = () => {
-  addingFormName.value = 'Название';
-  addingFormLinkAdress.value = 'Ссылка на картинку';
-  addingForm.classList.toggle('adding-form_status_active');
-  addingFormName.focus();
-};
-
-const addNewCard = (event) => {
-  event.preventDefault();
-  const card = galleryItem.cloneNode(true);
-  card.querySelector('.gallery-item').setAttribute('data-index', `${gallery.children.length}`);
-  card.querySelector('.gallery-item__image').setAttribute('src', `${addingFormLinkAdress.value}`);
-  card.querySelector('.gallery-item__image').setAttribute('alt', `${addingFormName.value}`);
-  card.querySelector('.gallery-item__title').textContent = addingFormName.value;
-  gallery.prepend(card);
-};
-
-const closeAddingForm = (event) => {
-  if (event.target === event.currentTarget || event.target === addingFormAddButton || event.target === addingFormCloseButton) {
-    addingForm.classList.toggle('adding-form_status_active');
-  }
-};
-profileAddingButton.addEventListener('click', openAddingForm);
-addingFormContainer.addEventListener('submit', addNewCard);
-addingForm.addEventListener('click', closeAddingForm);
-/* POPUP: Adding cards form - END */
-
-/* POPUP: Foto viewer */
-const fotoViewerImg = document.querySelector('.foto-viewer__img');
-const fotoViewer = document.querySelector('.foto-viewer');
-const closeFotoViewerButton = document.querySelector('.foto-viewer__close-button');
-const fotoCaption = document.querySelector('.foto-viewer__description');
-
-const closeFotoViewer = (event) => {
-  if (event.target === event.currentTarget || event.target === closeFotoViewerButton) {
-    fotoViewer.classList.toggle('foto-viewer_status_active');
-  }
-}
-
-const openFotoViewer = (event) => {
+const fillFotoViewer = (event) => {
   const linkAdress = event.target.getAttribute('src');
   const imgName = event.target.getAttribute('alt');
   fotoViewerImg.setAttribute('src', linkAdress);
   fotoViewerImg.setAttribute('alt', imgName);
   fotoCaption.textContent = imgName;
-  fotoViewer.classList.toggle('foto-viewer_status_active');
-}
+};
 
-fotoViewer.addEventListener('click', closeFotoViewer);
-/* Foto viewer - END */
+const openForm = (event) => {
+  if (event.target === profileEditButton) {
+    formFillValues(event);
+    toggleClass(editForm);
+  }
+  if (event.target === profileAddingButton) {
+    toggleClass(addingForm);
+  }
+  if (event.target.classList.contains('gallery-item__image')) {
+    fillFotoViewer(event);
+    toggleClass(fotoViewer);
+  }
+};
 
-/* Event cards */
+const closeForm = (event) => {
+  if (event.target === event.currentTarget || event.target === editSaveButton || event.target === addCardButton || event.target.classList.contains('form__close-button')) {
+    toggleClass(event.target.closest('.form'));
+  }
+};
+
+const createNewCard = (name, link) => {
+  const card = galleryItem.cloneNode(true);
+  const cardImg = card.querySelector('.gallery-item__image');
+  cardImg.setAttribute('src', `${link}`);
+  cardImg.setAttribute('alt', `${name}`);
+  card.querySelector('.gallery-item__title').textContent = name;
+  return card;
+};
+
+const submitInputChange = (event) => {
+  event.preventDefault();
+  if (event.target.classList.contains('form__container_type_edit')) {
+    fullNameOnPage.textContent = editFormFullName.value;
+    descriptionOnPage.textContent = editFormDescription.value;
+  }
+  if (event.target.classList.contains('form__container_type_adding')) {
+    gallery.prepend(createNewCard(addingFormName.value, addingFormLinkAdress.value));
+    addingFormName.value = '';
+    addingFormLinkAdress.value = '';
+  }
+};
+
 const eventCards = (event) => {
   if (event.target.classList.contains('gallery-item__heart-button')) {
     event.target.classList.toggle('gallery-item__heart-button_type_active');
   }
-
-  if (event.target.classList.contains('gallery-item__image')) {
-    openFotoViewer(event);
-  }
-
   if (event.target.classList.contains('gallery-item__trash-button')) {
     const listItem = event.target.closest('.gallery-item');
     listItem.remove();
   }
 };
 
-gallery.addEventListener('click', eventCards);
-/* Event cards - END */
-
-/* Init Cards in Gallary */
 const renderGalleryItems = () => {
-  initialCards.forEach((item, index) => {
-    const card = galleryItem.cloneNode(true);
-    card.querySelector('.gallery-item').setAttribute('data-index', `${index}`);
-    card.querySelector('.gallery-item__image').setAttribute('src', `${item.link}`);
-    card.querySelector('.gallery-item__image').setAttribute('alt', `${item.name}`);
-    card.querySelector('.gallery-item__title').textContent = item.name;
-    gallery.append(card);
+  initialCards.forEach((item) => {
+    gallery.append(createNewCard(item.name, item.link));
   });
 };
-/* Init Cards in Gallary - END */
+
+profileEditButton.addEventListener('click', openForm);
+profileAddingButton.addEventListener('click', openForm);
+gallery.addEventListener('click', openForm);
+editForm.addEventListener('click', closeForm);
+addingForm.addEventListener('click', closeForm);
+fotoViewer.addEventListener('click', closeForm);
+editForm.addEventListener('submit', submitInputChange);
+addingForm.addEventListener('submit', submitInputChange);
+gallery.addEventListener('click', eventCards);
 
 renderGalleryItems();
