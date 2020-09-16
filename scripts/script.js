d8d34a685be54850b4d5f44a15b6023612b2f48a
Artem Kulakov <kulakov22@yandex.ru>
2020-09-16 19:42:17 +0700

    hotfix: Исправить замечания после ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 import { initialCards } from './data.js';
 
 const gallery = document.querySelector('.gallery');
+const form = document.querySelectorAll('.form');
 const galleryItem = document.querySelector('#gallery-item').content;
 const fotoViewerImg = document.querySelector('.form__foto-viewer-img');
 const fotoCaption = document.querySelector('.form__foto-viewer-description');
@@ -13,79 +14,62 @@ const editFormFullName = document.querySelector('.form__textinput_type_edit-full
 const editFormDescription = document.querySelector('.form__textinput_type_edit-description');
 const fullNameOnPage = document.querySelector('.profile__full-name');
 const descriptionOnPage = document.querySelector('.profile__description');
-const editSaveButton = document.querySelector('.form__submit-button_type_edit-save-button');
-const addCardButton = document.querySelector('.form__submit-button_type_adding-add-button');
 const addingFormName = document.querySelector('.form__textinput_type_adding-name');
 const addingFormLinkAdress = document.querySelector('.form__textinput_type_adiing-link-adress');
 
-const toggleClass = (elem) => {
+const togglePopupStatus = (elem) => {
   elem.classList.toggle('form_status_active');
 };
 
-const formFillValues = () => {
-  editFormFullName.value = fullNameOnPage.textContent;
-  editFormDescription.value = descriptionOnPage.textContent;
-  editFormFullName.focus();
-};
-
-const fillFotoViewer = (event) => {
-  const linkAdress = event.target.getAttribute('src');
-  const imgName = event.target.getAttribute('alt');
-  fotoViewerImg.setAttribute('src', linkAdress);
-  fotoViewerImg.setAttribute('alt', imgName);
-  fotoCaption.textContent = imgName;
-};
-
-const openForm = (event) => {
-  if (event.target === profileEditButton) {
-    formFillValues(event);
-    toggleClass(editForm);
-  }
-  if (event.target === profileAddingButton) {
-    toggleClass(addingForm);
-  }
-  if (event.target.classList.contains('gallery-item__image')) {
-    fillFotoViewer(event);
-    toggleClass(fotoViewer);
-  }
-};
+const findParentForm = elem => elem.closest('.form');
 
 const closeForm = (event) => {
-  if (event.target === event.currentTarget || event.target === editSaveButton || event.target === addCardButton || event.target.classList.contains('form__close-button')) {
-    toggleClass(event.target.closest('.form'));
-  }
+  if (event.target === event.currentTarget || event.target.classList.contains('form__close-button')) {
+    togglePopupStatus(event.target.closest('.form'));
+  };
 };
 
 const createNewCard = (name, link) => {
   const card = galleryItem.cloneNode(true);
   const cardImg = card.querySelector('.gallery-item__image');
+  const cardLike = card.querySelector('.gallery-item__heart-button');
+  const cardTrash = card.querySelector('.gallery-item__trash-button');
+
   cardImg.setAttribute('src', `${link}`);
   cardImg.setAttribute('alt', `${name}`);
   card.querySelector('.gallery-item__title').textContent = name;
+
+  cardLike.addEventListener('click', (event) => {
+    event.target.classList.toggle('gallery-item__heart-button_type_active');
+  });
+  cardTrash.addEventListener('click', (event) => {
+    const listItem = event.target.closest('.gallery-item');
+    listItem.remove();
+  });
+  cardImg.addEventListener('click', (event) => {
+    const linkAdress = event.target.getAttribute('src');
+    const imgName = event.target.getAttribute('alt');
+    fotoViewerImg.setAttribute('src', linkAdress);
+    fotoViewerImg.setAttribute('alt', imgName);
+    fotoCaption.textContent = imgName;
+    togglePopupStatus(fotoViewer);
+  });
+
   return card;
 };
 
-const submitInputChange = (event) => {
+const submitEditForm = (event) => {
   event.preventDefault();
-  if (event.target.classList.contains('form__container_type_edit')) {
-    fullNameOnPage.textContent = editFormFullName.value;
-    descriptionOnPage.textContent = editFormDescription.value;
-  }
-  if (event.target.classList.contains('form__container_type_adding')) {
-    gallery.prepend(createNewCard(addingFormName.value, addingFormLinkAdress.value));
-    addingFormName.value = '';
-    addingFormLinkAdress.value = '';
-  }
+  fullNameOnPage.textContent = editFormFullName.value;
+  descriptionOnPage.textContent = editFormDescription.value;
+  togglePopupStatus(findParentForm(event.target));
 };
 
-const eventCards = (event) => {
-  if (event.target.classList.contains('gallery-item__heart-button')) {
-    event.target.classList.toggle('gallery-item__heart-button_type_active');
-  }
-  if (event.target.classList.contains('gallery-item__trash-button')) {
-    const listItem = event.target.closest('.gallery-item');
-    listItem.remove();
-  }
+const submitAddingForm = (event) => {
+  event.preventDefault();
+  gallery.prepend(createNewCard(addingFormName.value, addingFormLinkAdress.value));
+  event.target.closest('.form__container').reset();
+  togglePopupStatus(findParentForm(event.target));
 };
 
 const renderGalleryItems = () => {
@@ -94,14 +78,17 @@ const renderGalleryItems = () => {
   });
 };
 
-profileEditButton.addEventListener('click', openForm);
-profileAddingButton.addEventListener('click', openForm);
-gallery.addEventListener('click', openForm);
-editForm.addEventListener('click', closeForm);
-addingForm.addEventListener('click', closeForm);
-fotoViewer.addEventListener('click', closeForm);
-editForm.addEventListener('submit', submitInputChange);
-addingForm.addEventListener('submit', submitInputChange);
-gallery.addEventListener('click', eventCards);
+form.forEach((form) => {
+  form.addEventListener('click', closeForm);
+});
+profileEditButton.addEventListener('click', () => {
+    togglePopupStatus(editForm);
+    editFormFullName.value = fullNameOnPage.textContent;
+    editFormDescription.value = descriptionOnPage.textContent;
+    editFormFullName.focus();
+});
+profileAddingButton.addEventListener('click', () => togglePopupStatus(addingForm));
+editForm.addEventListener('submit', submitEditForm);
+addingForm.addEventListener('submit', submitAddingForm);
 
 renderGalleryItems();
