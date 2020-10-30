54a389b94407053b1004f90995d81c24192a8fbf
Artem Kulakov <kulakov22@yandex.ru>
2020-10-30 13:00:03 +0700

    hotfix: delete source map string

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/pages/index.js',
-  devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
     publicPath: './',
